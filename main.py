87e8a8eccde7bcb4e43b61a5a8f8256e2d87047c
Richard Putra <richardputra2910@gmail.com>
2021-09-03 22:40:01 +0700

    work around discord linux async

--- main.py
+++ main.py
@@ -16,11 +16,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = Client('starting...', channel_name='badger-bot')
-t = threading.Thread(name='', target=lambda: client.run(os.getenv('DISCORD_TOKEN')))
-t.setDaemon(True)
-t.start()
-while not client.started:
-	time.sleep(0.1)
 
 def send_alarm(b: Badger, next: Optional[Badger]=None, channel='badger-bot'):
 	client.send_message(
@@ -44,45 +39,54 @@ try:
 except Exception as e:
 	print(stack_trace(e))
 
-while True:
-	badgers, err = Scraper.get_badgers()
-	if err != None:
-		print(err)
-		continue
-	rares = {a: [] for a, p in percentages.items() if p <= 1.5}
-	for b in badgers:
-		rare_count = 0
-		for i, a in enumerate(b.attributes_list):
-			if a == '':
-				print(f'this one right here `{b.attributes}` {b.name}')
-				continue
-			if a not in percentages:
-				print(f'missing attribute: `{a}` {b.name}')
-				continue
-			if percentages[a] <= 1.5:
-				rares[a].append(copy.deepcopy(b))
-				rare_count += 1
-		if rare_count > 1:
-			if (b.id, b.seller_address) not in sent:
-				send_alarm(b)
-				sent.add((b.id, b.seller_address))
-				with open('sent.pkl', 'wb') as f:
-					pickle.dump(sent, f)
-		if len(b.attributes_list) in [1, 9]:
-			if (b.id, b.seller_address) not in sent:
-				send_alarm(b, channel='badger0178')
-				sent.add((b.id, b.seller_address))
-				with open('sent.pkl', 'wb') as f:
-					pickle.dump(sent, f)
-			
-	for a, badgers in rares.items():
-		if len(badgers) < 2:
+def start():
+	while not client.started:
+		time.sleep(0.1)
+
+	while True:
+		badgers, err = Scraper.get_badgers()
+		if err != None:
+			print(err)
 			continue
-		sorted_b = sorted(badgers, key=lambda x: x.price)
-		b = sorted_b[0]
-		if b.price <= 0.7 * sorted_b[1].price and (b.id, b.seller_address) not in sent:
-			send_alarm(b, sorted_b[1], 'badger-bot-cheap-alarm')
-			sent.add((b.id, b.seller_address))
-			with open('sent.pkl', 'wb') as f:
-				pickle.dump(sent, f)
-	
+		rares = {a: [] for a, p in percentages.items() if p <= 1.5}
+		for b in badgers:
+			rare_count = 0
+			for i, a in enumerate(b.attributes_list):
+				if a == '':
+					print(f'this one right here `{b.attributes}` {b.name}')
+					continue
+				if a not in percentages:
+					print(f'missing attribute: `{a}` {b.name}')
+					continue
+				if percentages[a] <= 1.5:
+					rares[a].append(copy.deepcopy(b))
+					rare_count += 1
+			if rare_count > 1:
+				if (b.id, b.seller_address) not in sent:
+					send_alarm(b)
+					sent.add((b.id, b.seller_address))
+					with open('sent.pkl', 'wb') as f:
+						pickle.dump(sent, f)
+			if len(b.attributes_list) in [1, 9]:
+				if (b.id, b.seller_address) not in sent:
+					send_alarm(b, channel='badger0178')
+					sent.add((b.id, b.seller_address))
+					with open('sent.pkl', 'wb') as f:
+						pickle.dump(sent, f)
+				
+		for a, badgers in rares.items():
+			if len(badgers) < 2:
+				continue
+			sorted_b = sorted(badgers, key=lambda x: x.price)
+			b = sorted_b[0]
+			if b.price <= 0.7 * sorted_b[1].price and (b.id, b.seller_address) not in sent:
+				send_alarm(b, sorted_b[1], 'badger-bot-cheap-alarm')
+				sent.add((b.id, b.seller_address))
+				with open('sent.pkl', 'wb') as f:
+					pickle.dump(sent, f)
+		
+t = threading.Thread(name='', target=start)
+t.setDaemon(True)
+t.start()
+
+client.run(os.getenv('DISCORD_TOKEN'))
